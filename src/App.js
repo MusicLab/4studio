c54ec7bf2ddd5427d8de567a4a158f575450e285
MusicLab <baruffa_89@hotmail.com>
2024-03-13 13:07:00 -0300

    13-3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './assets/logo.png'; // Ruta a tu archivo de imagen del logo
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 
 
 function App() {
+  const [showDiv, setShowDiv] = useState(false);
+
+  useEffect(() => {
+    setShowDiv(true);
+  }, []);
   return (
     <div className="App">
 
@@ -15,30 +21,33 @@ function App() {
         <div class="leyenda">Diseñamos y construimos stands</div>
 
         <div class="contacto">
-          <a href="#" class="button">Contacto</a>
+          <a href="#" class="button">CONTACTO</a>
         </div>
-        <div class="contenedor">
-          <div class="izquierda">
-            <div class= "blanco-ofi">Oficina</div>
-            <ul class="sin-puntos">
-              <li class= "blanco">Behance</li>
-              <li class= "blanco">Instagram</li>
-              <li class= "blanco">E-mail</li>
-            </ul>
-          </div>
-          <div class="derecha">
-            <div class= "blanco-dire">Arribeños 3230 Piso 3 depto 9</div>
-            <ul class="sin-puntos">
-              <li class= "blanco">
-                <a href="https://www.behance.net/studio4ss">@STUDIO4</a>
-              </li>
-              <li class= "blanco">
-                <a href="https://www.instagram.com/studio_4ss/">STUDIO_4ss</a>
-              </li>
-              <li class= "blanco">
-                <a href="mailto:Hola@studio4ss.com">roxana@studio4ss.com</a>
-              </li>
-            </ul>
+
+        <div className={`slide-from-bottom ${showDiv ? 'visible' : ''}`}>
+          <div class="contenedor">
+            <div class="izquierda">
+              <div class= "blanco-ofi">Oficina</div>
+              <ul class="sin-puntos">
+                <li class= "blanco">Behance</li>
+                <li class= "blanco">Instagram</li>
+                <li class= "blanco">E-mail</li>
+              </ul>
+            </div>
+            <div class="derecha">
+              <div class= "blanco-dire">Arribeños 3230 Piso 3 dpto 9</div>
+              <ul class="sin-puntos">
+                <li class= "blanco">
+                  <a href="https://www.behance.net/studio4ss">@studio4</a>
+                </li>
+                <li class= "blanco">
+                  <a href="https://www.instagram.com/studio_4ss/">studio_4ss</a>
+                </li>
+                <li class= "blanco">
+                  <a href="mailto:Hola@studio4ss.com">roxana@studio4ss.com</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
 
